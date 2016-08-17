b16d86772273a76f59abe16165c1cb5dae94f366
niling <lin_ni@shyms.org>
2016-08-17 14:02:29 +0800

    。

--- WindowsServiceTest/WindowsServiceTest/Install.bat
+++ WindowsServiceTest/WindowsServiceTest/Install.bat
@@ -1,4 +1,4 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe E:\WindowsServiceTest\WindowsServiceTest\WindowsServiceTestUI\bin\Debug\Service\WindowsServiceTest.exe
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe E:\WindowsService\WindowsServiceTest\WindowsServiceTestUI\bin\Debug\Service\WindowsServiceTest.exe
 Net Start ServiceTest
 sc config ServiceTest start= auto
 pause
--- WindowsServiceTest/WindowsServiceTestUI/bin/Debug/Service/Uninstall.bat
+++ WindowsServiceTest/WindowsServiceTestUI/bin/Debug/Service/Uninstall.bat
@@ -1,2 +1,2 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /u WindowsServiceTest.exe
-::pause
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /u E:\WindowsService\WindowsServiceTest\WindowsServiceTestUI\bin\Debug\Service\WindowsServiceTest.exe
+pause
