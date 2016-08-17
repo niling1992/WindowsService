3545dc0d6c80e1cff0fe2971e2158a378e865f9c
niling <lin_ni@shyms.org>
2016-08-17 15:43:35 +0800

    2

--- WindowsService/YmsWindowsService/bin/Debug/Service/Install.bat
+++
@@ -1,5 +0,0 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe E:\WindowsService\WindowsServiceTest\WindowsServiceTestUI\bin\Debug\Service\WindowsServiceTest.exe
-Net Start ServiceTest
-sc config ServiceTest start= auto
-pause
-asdsadsa
