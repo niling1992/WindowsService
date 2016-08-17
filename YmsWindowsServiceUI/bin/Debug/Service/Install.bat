5f1bbf5c01000b4f79165f6dd1f462d08387be2c
niling <lin_ni@shyms.org>
2016-08-17 17:43:36 +0800

    1

--- YmsWindowsServiceUI/bin/Debug/Service/Install.bat
+++ YmsWindowsServiceUI/bin/Debug/Service/Install.bat
@@ -1,4 +1,4 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe  YmsWindowsService.exe
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe  "%~dp0\YmsWindowsService.exe"
 Net Start YmsService
 sc config YmsService start= auto
 pause
