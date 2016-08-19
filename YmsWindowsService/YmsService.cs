f13a17e45deb8e0104efb4b4253702393bf28592
niling <lin_ni@shyms.org>
2016-08-19 17:54:58 +0800

    1

--- YmsWindowsService/YmsService.cs
+++ YmsWindowsService/YmsService.cs
@@ -24,27 +24,23 @@ namespace YmsWindowsService
         protected override void OnStart(string[] args)
         {
 
-            using (StreamWriter sw = new StreamWriter("C:\\log.txt", true))
+            //定时器
+            Timer MT = new Timer();
+
+            MT.Enabled = true;
+            MT.Interval = (3600000);//每小时运行一次
+
+            MT.Elapsed += new System.Timers.ElapsedEventHandler((object sender, ElapsedEventArgs e) =>
             {
-                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss "));
-            }
-            ////定时器
-            //Timer MT = new Timer();
-
-            //MT.Enabled = true;
-            //MT.Interval = (3600000);//每小时运行一次
-
-            //MT.Elapsed += new System.Timers.ElapsedEventHandler((object sender, ElapsedEventArgs e) =>
-            //{
-            //    using (StreamWriter sw = new StreamWriter("C:\\log.txt", true))
-            //    {
-            //        sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss "));
-            //    }
-            //    /*获取以发送短信到区政府平台间隔大于一小时的数据*/
-            //    var list = msg_ccsms_sendDAL.GetSqlList();
-            //    var http = new EasyHttp.Http.HttpClient();
-            //    http.Post("url", list, EasyHttp.Http.HttpContentTypes.ApplicationJson);
-            //});//每小时执行一次
+                using (StreamWriter sw = new StreamWriter("C:\\log.txt", true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss "));
+                }
+                /*获取以发送短信到区政府平台间隔大于一小时的数据*/
+                var list = msg_ccsms_sendDAL.GetSqlList();
+                var http = new EasyHttp.Http.HttpClient();
+                http.Post("url", list, EasyHttp.Http.HttpContentTypes.ApplicationJson);
+            });//每小时执行一次
         }
 
 
